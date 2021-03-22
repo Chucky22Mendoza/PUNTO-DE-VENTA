8bfdfb91bd4c122b9ab1b4c8e05346351a0fb7e1
Jessy Avalos Barrales <jessyavalos16@outlook.es>
2021-03-22 09:11:32 -0600

    Revert "Revert "actualizacion actomatica removida añadido de busqueda por nombre del producto, correccion en el actualizado de inventario""
    This reverts commit b38771c20d79b2748a479136c9c5533066366427.

--- Logic/LogicInventario.cs
+++ Logic/LogicInventario.cs
@@ -33,28 +33,31 @@ namespace Logic {
                 listLabel[0].Text = "Código del producto";
                 listLabel[0].ForeColor = Color.Black;
                 if (numInventario.Value > 0) {
-                    listLabel[5].Text = "Cantidad";
-                    listLabel[5].ForeColor = Color.Black;
                     var producto = _Producto.Where(obj => obj.codigo.Equals(txtCodigoProducto.Text)).ToList();
                     if (producto.Count.Equals(1)) {
+
                         listLabel[0].Text = "Código del producto";
                         listLabel[0].ForeColor = Color.Black;
                         guardarRegistro();
+
                     } else {
+
                         listLabel[0].Text = "Código del producto existente requerido";
                         listLabel[0].ForeColor = Color.Red;
                         txtCodigoProducto.Focus();
+
                     }
                 } else {
-                    listLabel[5].Text = "Cantidad mayor a 0 requerida";
-                    listLabel[5].ForeColor = Color.Red;
-                    listLabel[5].Location = new Point(75, 214);
+
                     numInventario.Focus();
+
                 }
             } else {
+
                 listLabel[0].Text = "Código del producto requerido";
                 listLabel[0].ForeColor = Color.Red;
                 txtCodigoProducto.Focus();
+
             }
         }
 
@@ -69,28 +72,25 @@ namespace Logic {
             if (query.Count.Equals(1)) {
                 query.ForEach(obj => {
                     listLabel[2].Text = obj.descripcion;
-                    listLabel[4].Text = obj.existencia.ToString();
+                    numInventario.Value = Convert.ToDecimal(obj.existencia.ToString());
                 });
             } else {
                 listLabel[2].Text = "-";
-                listLabel[4].Text = "0";
+
+                numInventario.Value = 0;
             }
         }
 
-        int nuevaExistencia;
         private void guardarRegistro() {
+
             BeginTransactionAsync();
             try {
-                List<Producto> productos = _Producto.Where(obj => obj.codigo.Equals(txtCodigoProducto.Text)).ToList();
-                productos.ForEach(element => {
-                    nuevaExistencia = (int)(element.existencia + Convert.ToInt32(numInventario.Value));
-                });
                 
                 _Producto.Where(obj => obj.codigo.Equals(txtCodigoProducto.Text))
-                    .Set(obj => obj.existencia, nuevaExistencia)
+                    .Set(obj => obj.existencia, Convert.ToInt32(numInventario.Value))
                     .Update();
 
-                MessageBox.Show("Producto actualizado correctamente", "Información", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                MessageBox.Show("Producto actualizado correctamente", "Guardado Exitoso", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 limpiarCampos();
                 CommitTransaction();
             } catch (SqlException) {
@@ -375,13 +375,10 @@ namespace Logic {
 
         private void limpiarCampos() {
             listLabel[2].Text = "-";
-            listLabel[4].Text = "0";
+            numInventario.Value = 0;
 
             listLabel[0].Text = "Código del producto";
             listLabel[0].ForeColor = Color.Black;
-            listLabel[5].Text = "Cantidad";
-            listLabel[5].ForeColor = Color.Black;
-            listLabel[5].Location = new Point(146, 214);
             txtCodigoProducto.Text = "";
             numInventario.Value = 0;
         }
---
+++ Punto de Venta/BuscarProductos.cs
@@ -0,0 +1,59 @@
+﻿using Data;
+using Data.Model;
+using Logic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punto_de_Venta
+{
+    public partial class BuscarProductos : Form
+    {
+
+        private LogicProductos productos;
+        private LogicVenta ventas = new LogicVenta();
+
+        List<Producto> producto = new List<Producto>();
+
+        public BuscarProductos()
+        {
+            InitializeComponent();
+
+            productos = new LogicProductos(tableSearchProducts);
+        }
+
+        private void txtProductName_TextChanged(object sender, EventArgs e)
+        {
+
+            if (txtProductName.Text != "")
+            {
+
+                productos.searchProduct(txtProductName.Text);
+
+            }
+
+        }
+
+        private void btnSelectProduct_Click(object sender, EventArgs e)
+        {
+
+            if (tableSearchProducts.SelectedRows.Count != 0)
+            {
+
+                DataGridViewRow data = this.tableSearchProducts.SelectedRows[0];
+
+                Main.productoCodigo = (string)data.Cells["codigo"].Value;
+
+                this.Close();
+
+            }
+
+        }
+    }
+}
